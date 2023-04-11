5f462d269e5ec5e374dff0d5b9d9093647764012
Giacomo Pessotto <giacomopesso@gmail.com>
2023-04-11 18:27:55 +0100

    fixed db connection

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: "postgres",
-    password: "1T9mACVXDhPuFOce064v",
-    host: "containers-us-west-160.railway.app",
-    port: 5835,
-    database: "railway"
+    user: "USER",
+    password: "PASSWORD",
+    host: "HOST",
+    port: 0000,
+    database: "DATABASE"
 })
 
 module.exports = pool;
